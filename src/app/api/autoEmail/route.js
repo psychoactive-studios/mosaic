41bf13c4295d174e34083228323306fe2a3ef013
Joshvdw <joshvdw@live.com>
2024-04-02 12:50:56 +1300

    email api fixed func name

--- src/app/api/autoEmail/route.js
+++ src/app/api/autoEmail/route.js
@@ -5,34 +5,34 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 export async function POST(req) {
   const { _type, comment, email, fullName, approvalStatus } = await req.json();
 
-  if (_type === "comment" && approvalStatus === true) {
-    const message = {
-      to: email,
-      from: "josh@psychoactive.co.nz", // change to ara taiohi later
-      subject: "Your comment on Mosaic Cards has been approved!",
-      text: `Hello ${fullName}, we are just lettting you know that your comment: "${comment}" has been approved. Thank you for contributing!`,
-    };
-    try {
-      await sendgrid.send(message);
-      return new Response(
-        JSON.stringify({ message: "Email sent successfully" }),
-        {
-          status: 200,
-          headers: { "Content-Type": "application/json" },
-        }
-      );
-    } catch (error) {
-      console.error(error);
-      return new Response(
-        JSON.stringify({
-          error: "Failed to send email",
-          details: error.toString(),
-        }),
-        {
-          status: 500,
-          headers: { "Content-Type": "application/json" },
-        }
-      );
-    }
+  // if (_type === "comment" && approvalStatus === true) {
+  const message = {
+    to: email,
+    from: "josh@psychoactive.co.nz", // change to ara taiohi later
+    subject: "Your comment on Mosaic Cards has been approved!",
+    text: `Hello ${fullName}, we are just lettting you know that your comment: "${comment}" has been approved. Thank you for contributing!`,
+  };
+  try {
+    await sendgrid.send(message);
+    return new Response(
+      JSON.stringify({ message: "Email sent successfully" }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({
+        error: "Failed to send email",
+        details: error.toString(),
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
+  // }
 }
