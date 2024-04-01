6d62642d980d18c37252601c63b068f6c2682bcc
Joshvdw <joshvdw@live.com>
2024-04-02 11:35:03 +1300

    email api advanced error handle

--- src/app/api/autoEmail/route.js
+++ src/app/api/autoEmail/route.js
@@ -24,6 +24,9 @@ export default async function handler(req, res) {
       res.status(200).json({ message: "Email sent successfully" });
     } catch (error) {
       console.error(error);
+      if (error.response) {
+        console.error(error.response.body);
+      }
       return res.status(500).json({ error: "Failed to send email" });
     }
   } else {
