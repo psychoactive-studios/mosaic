ce91ee81392e5b146ec4eb1fcf6c436c38fc3f13
Joshvdw <joshvdw@live.com>
2024-04-04 15:44:42 +1300

    changed form api fields and worked on tooltips

--- src/app/api/autoEmail/route.js
+++ src/app/api/autoEmail/route.js
@@ -4,6 +4,7 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 export async function POST(req) {
   const { comment, email, fullName, pathway } = await req.json();
+  const url = "https://arataiohi.org.nz/";
 
   const message = {
     to: email,
@@ -11,7 +12,7 @@ export async function POST(req) {
     subject: "Your comment on Mosaic Cards has been approved!",
     // text: `Kia ora ${fullName}, we are just lettting you know that your comment: "${comment}" has been approved. Thank you for contributing!`,
     text: `Kia ora ${fullName},
-          Great news! Your feedback on the ${pathway} has been approved and added to the Mosaic website at: ${window.location.href}. We’re thrilled to share your comments with the community.
+          Great news! Your feedback on the ${pathway} has been approved and added to the Mosaic website at: ${url}. We’re thrilled to share your comments with the community.
           ${comment}
           It’s voices like yours that fuel our journey towards a more understanding, inclusive, and vibrant Aotearoa. 
           Thanks for being a part of this conversation and helping us all grow together.
