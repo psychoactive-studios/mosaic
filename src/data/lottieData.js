18bc800d4476cb0b318a396e3308c6e997466ad2
Joshvdw <joshvdw@live.com>
2024-04-04 14:35:39 +1300

    new lottie ui

--- src/data/lottieData.js
+++ src/data/lottieData.js
@@ -1,5 +1,5 @@
 export const lottieData = {
   preloader: "lotties/MOSAIC_export_test_v01.json",
-  next: "lotties/ui/next_v04A.json",
+  next: "lotties/ui/next_v05A.json",
   prev: "lotties/prev.json",
 };
